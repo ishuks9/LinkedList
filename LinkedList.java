1c48bbfe60d63d4a7286b639d75a78272a8e29f1
ishuks9 <ishuks9@gmail.com>
2021-07-19 18:55:18 +0530

    updated the UC5

--- LinkedList.java
+++ LinkedList.java
@@ -25,6 +25,42 @@ public class LinkedList<T> {
 		size++;
 	}
 	
+	public void removeAt(int pos){
+		int tempPos=0;
+		Node<T> tempPrev=root; 
+		Node<T> tempCurrent=root;
+		if(pos==0){
+			root=root.next;
+			
+		}else if(pos==(this.size()-1)){
+			while(tempCurrent.next!=null){
+				tempPrev=tempCurrent;
+				tempCurrent=tempCurrent.next;
+			}
+			tempPrev.next = tempPrev.next.next;
+			end=tempPrev;
+		}else if(pos<this.size()){
+			while(tempCurrent.next!=null){
+				if(pos==tempPos){	
+					tempPrev.next=tempPrev.next.next;
+					break;
+				}
+				tempPrev=tempCurrent;
+				tempCurrent=tempCurrent.next;
+				tempPos++;
+			}
+		}else{
+			System.out.println("Given index does not exist");
+		}
+		
+	}
+	//Delete first element
+	public T pop(){
+		T data=end.data;
+		removeAt(this.size()-1);
+		return data;
+	}
+	
 	public T get(int index){
 		Node<T> tempCurrent=new Node(null);
 		if(index>=0 && index<this.size()){
