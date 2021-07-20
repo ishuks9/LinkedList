bed36bd275ee004ae0f1f00d0e1bd887183c4b7c
ishuks9 <ishuks9@gmail.com>
2021-07-20 15:25:32 +0530

    updated the UC10

---
+++ OrderedListMain.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.linkedlist;
+
+public class OrderedListMain {
+
+	public static void main(String[] args) {
+		
+		SortedLinkedList<Integer> list=new SortedLinkedList<Integer>();
+		 list.push(56);
+		 list.push(30);
+		 list.push(40);
+		 list.push(70);
+                 list.printList();		 
+	}
+
+}
