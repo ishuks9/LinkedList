1a113062c3756d05a40e2047619bf65a596019fa
ishuks9 <ishuks9@gmail.com>
2021-07-20 14:22:12 +0530

    updated the UC6

--- Main.java
+++ Main.java
@@ -7,8 +7,8 @@ public class Main {
 		    List.push(56);
 		    List.push(30);
 		    List.push(70);
-                    List.get(2);
-                    List.pop();
+                    List.printList();
+                    List.popLast();
 
 		    List.printList();
 
