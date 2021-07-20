233f55bf7c8af343178ed0aaa6714f5bba242887
ishuks9 <ishuks9@gmail.com>
2021-07-20 14:45:48 +0530

    updated the UC8

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main {
   public static void main(String[] args) {
 		 LinkedList List = new LinkedList();
 
-		    //Add four elements in the list.
+		    //Add elements in the list.
 		    List.push(56);
 		    List.push(30);
 		    List.push(70);
